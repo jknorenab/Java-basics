9de7245c27defac93110ff571750a9c10b555468
jknorenab <jknorenab@unal.edu.co>
2021-01-06 21:46:00 -0500

    Se actualiza clase: Color.
    Se añaden métodos getter and setter, como también el método colorSet

--- CajonFiguras/source/cajon/mundo/Color.java
+++ CajonFiguras/source/cajon/mundo/Color.java
@@ -36,7 +36,7 @@ public class Color {
 	 */
 	public Color(int pR, int pG, int pB) {
 		r = pR;
-		g = pG;
+		g = pG; //Porqué no ponemos en estos métodos this.r. etc...
 		b = pB;
 	}
 	
@@ -48,4 +48,42 @@ public class Color {
 	//     un array primitivo de tamaño 3 con los atributos r, g, b
 	//     Para mayor información sobre el modelo RGB revisar
 	//     https://es.wikipedia.org/wiki/RGB
+	
+	//Métodos getter & setter ~Solución
+	
+	public int getR(){  // ~Recuerda que los métodos deben ser públicos para que otras clases tengan la capacidad de preguntarle a esta clase y esta, de dar su respuesta
+		return this.r;
+	}
+	
+	public int getG() {
+		return this.g;
+	}
+	
+	public int getB() {
+		return this.b;
+	}
+	
+	public void setR(int pR) { // Es void porque no retorna nada, sólo cambia el valor del parámetro
+		this.r = pR;
+	}
+	
+	public void setG(int pG) {
+		this.g = pG;
+	}
+	
+	public void setB(int pB) {
+		this.b = pB;
+	}
+	
+	// Recuerda que se pueden añadir automáticamente los métodos getter y setter, yendo a Source/Generate Getters and Setters
+	// :) <3
+	
+	public int[] colorSet(int pR, int pG, int pB) {
+		int[] colorsettings = new int[3];
+		colorsettings[0] = pR;
+		colorsettings[1] = pG;
+		colorsettings[2] = pB;
+		return colorsettings;
+	}
+	
 }
