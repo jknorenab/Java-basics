d242c0b075fbebe70421bb84d9df164fabdee595
jknorenab <jknorenab@unal.edu.co>
2021-01-08 20:27:22 -0500

    Se actualiza clase CajonFiguras
    Se cambian de públicos a privados los métodos de lectura de archivos, se
    crea función para obtener los 3 perímetros más grandes, y se comentan
    funciones adicionales

--- CajonFiguras/source/cajon/mundo/CajonFiguras.java
+++ CajonFiguras/source/cajon/mundo/CajonFiguras.java
@@ -7,6 +7,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
 
+import javax.swing.*;
+
 
 /**
  * Clase que modela un cajón de figuras.
@@ -27,6 +29,8 @@ public class CajonFiguras {
 	public CajonFiguras() {
 		// El constructo de la calse genera un ArrayList de figuras  vacío, sin recibir nada por parámetro
 		figuras = new ArrayList<Figura>();
+		cargarFiguras(); 
+		cargarTriangulos();
 	}
 	
 	
@@ -35,6 +39,10 @@ public class CajonFiguras {
 	//----------------
 	
 	// No es necesario crear métodos getter y setter en este caso.
+	
+	public ArrayList darFiguras() {
+		return figuras;
+	}
 	
 	/**
 	 * Determina la figura con mayor perímetro dentro del ArrayList figuras.
@@ -171,7 +179,7 @@ public class CajonFiguras {
 	 * Método para cargar la información del archivo triangulos.csv
 	 */
 		
-	public void cargarTriangulos () {
+	private void cargarTriangulos () {
 		try {
 			BufferedReader br = new BufferedReader( new FileReader("data/triangulos.csv") );
 			String linea;
@@ -199,7 +207,15 @@ public class CajonFiguras {
 		
 	}
 	
-	public void cargarFiguras() {
+	/**
+	 * Lee los datos de un archivo csv, 
+	 * crea las figuras correspondientes y las añade al ArrayList de figuras
+	 */
+	
+	// Los métodos de lectura no necesariamente deben ser públicos. Principio de encasuplación
+	// Dado que, no es necesario que otras clases llamen este método. 
+	
+	private void cargarFiguras() {
 		try {
 			BufferedReader br = new BufferedReader( new FileReader("data/figuras.csv") );
 			String linea;
@@ -226,10 +242,34 @@ public class CajonFiguras {
 		}	
 		
 	}
+	/**
+	 * Este método crea un arreglo en el que las figuras están ordenadas de mayor a menor perímetro
+	 * @return  String Mensaje con los valores de los tres mayores perímetros
+	 */
+	
+	public String figurasConMayorPerimetro() {
+		// Clona el vector figuras asegurándose que lo convierte en un array list
+		// Esto para tener un arreglo de figuras ordenadas y uno con las figuras tal y cómo se cargaron
+		// Es decir, no afectamos el original
+		ArrayList<Figura> figurasOrdenadas = (ArrayList<Figura>) figuras.clone();
+		//Ordeno las figuras según su perímetro. 
+		// Este método, va a figura y le pregunta usted cómo compara? 
+		//Para eso creamos el metodo compareTo en figura
+		//Luego, con esa característica los ordena de mayor a menor y los añade al arreglo
+		Collections.sort(figurasOrdenadas);
+		//Collections.reverse(figuras); Para ordenarlos de menor a mayor
+		// Obtenemos las primeras tres posiciones del array
+		// Las cuales albergan los tres mayores perímetros
+		String mensaje = figurasOrdenadas.get(0).calcularPerimetro() + "\n"; 
+		// Al Añadir las comillas al final hacemos cast, convirtiendo double en string
+		mensaje += figurasOrdenadas.get(1).calcularPerimetro() + "\n ";
+		mensaje += figurasOrdenadas.get(2).calcularPerimetro() + "\n ";
+		return mensaje;
+	}
 
 	
-	
-	
+	// |8 |7|9  a>b, a<b a=b c>b 
+ 	
 	//-----------------
 	// Main del proyecto
 	//-----------------
@@ -239,9 +279,10 @@ public class CajonFiguras {
 	 * @param args No es necesario.
 	 */
 	public static void main(String[] args) {
+		//Alguien dentro del main no puede llamar un método que sea privado.
+		//Este main en particular, puede llamar los métodos privados porque está dentro del
+		//mismo archivo en el que se definen los métodos privados. No debería hacerlo, pero puede.
 		CajonFiguras cajon = new CajonFiguras();
-		cajon.cargarTriangulos();
-		cajon.cargarFiguras();
 		System.out.println(cajon.figuraConMayorPerimetro());
 		Figura figura1 = new Figura();
 		System.out.println(figura1);
@@ -255,6 +296,7 @@ public class CajonFiguras {
 		System.out.println("Hay " + cajon.calcularNumFiguras() + " figuras.");
 		System.out.println(cajon.figuraConMayorPerimetro());
 		System.out.println(cajon.figuraConMenorPerimetro());
+		JOptionPane.showMessageDialog(null, "Holi :)");
 		
 		
 	}
