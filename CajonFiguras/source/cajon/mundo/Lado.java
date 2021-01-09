ec6e8b43625b4c79c1684d1a5202050aa597688b
jknorenab <jknorenab@unal.edu.co>
2021-01-08 20:25:47 -0500

    Se actualiza clase Lado
    Se comentan los métodos y resuelven dudas previas

--- CajonFiguras/source/cajon/mundo/Lado.java
+++ CajonFiguras/source/cajon/mundo/Lado.java
@@ -24,14 +24,16 @@ public class Lado {
 	 */
 	public Lado() {
 		color = new Color();
-		
-		//TODO: Todo lado tiene dos puntos. Cree una constante llamada
-		//      PUNTOS_POR_LADO y asígnela al crear el array de puntos.
 		puntos = new Punto[PUNTOS_POR_LADO];
+		//longitud = calcularLongitud();
+		//Si pusiéramos calcular longitud aquí arriba, habría un error
+		// Puesto que calcular longitud depende de que existan los puntos
 		puntos[0] = new Punto(0,0);
 		puntos[1] = new Punto(1,1);
-		longitud = calcularLongitud(); // Esto me confunde, aquí no importa la linealidad? Puedo llamar a calcularLongitud sin haberla definido antes?
-	}
+		// Aquí puedo llamar a calcularLongitud sin haberla definido antes?
+		// Sí, porque antes definimos y creamos el arreglo puntos, del cual depende la función/método calcularLongitud
+		longitud = calcularLongitud(); 
+	} 
 	
 	/**
 	 * Construye un lado de color negro con los puntos recibidos por parámetros
@@ -40,8 +42,7 @@ public class Lado {
 	 */
 	public Lado(Punto[] pPuntos) {
 		color = new Color();
-		//TODO: Aquí falta algo por hacer para el array puntos. ¿Qué será?
-		//      Hint: Mi error en la clase al construir el array.
+		// Asignamos el arreglo puntos a un nuevo arreglo de Puntos de tamaño Puntos_POR_LADO
 		puntos = new Punto[PUNTOS_POR_LADO];
 		puntos = pPuntos;
 		longitud = calcularLongitud();
@@ -54,8 +55,7 @@ public class Lado {
 	 * @param pColor Color en código RGB
 	 */
 	public Lado(Punto[] pPuntos, Color pColor) {
-		//TODO: Aquí falta algo por hacer para el array puntos. ¿Qué será?
-		//      Hint: Mi error en la clase al construir el array. 
+		// Asignamos el arreglo puntos a un nuevo arreglo de Puntos de tamaño Puntos_POR_LADO
 		puntos = new Punto[PUNTOS_POR_LADO];
 		//Aquí, debemos poner un if para verificar que los puntos sean de la misma dimensión?
 		puntos = pPuntos;
@@ -70,13 +70,12 @@ public class Lado {
 	 * @param punto2 Vértice del lado
 	 */
 	public Lado(Punto punto1, Punto punto2) {
-		//TODO: Aquí falta algo por hacer para el array puntos. ¿Qué será?
-		//      Hint: Mi error en la clase al construir el array.
+		// Asignamos el arreglo puntos a un nuevo arreglo de Puntos de tamaño Puntos_POR_LADO
 		puntos = new Punto[PUNTOS_POR_LADO];
 		puntos[0] = punto1;
 		puntos[1] = punto2;
 		color = new Color();
-		//TODO: Aquí falta hacer algo con longitud. Hint: revise los métodos anteriores.
+		// Asignamos el atributo longitud por medio de la función calcularLongitud
 		longitud = calcularLongitud();
 	}
 	
@@ -87,13 +86,12 @@ public class Lado {
 	 * @param punto2 Vértice del lado
 	 */
 	public Lado(Punto punto1, Punto punto2, Color pColor) {
-		//TODO: Aquí falta algo por hacer para el array puntos. ¿Qué será?
-		//      Hint: Mi error en la clase al construir el array.
+		// Asignamos el arreglo puntos a un nuevo arreglo de Puntos de tamaño Puntos_POR_LADO
 		puntos = new Punto[PUNTOS_POR_LADO];
 		puntos[0] = punto1;
 		puntos[1] = punto2;
 		color = pColor;
-		//TODO: Aquí falta hacer algo con longitud. Hint: revise los métodos anteriores.
+		// Asignamos el atributo longitud por medio de la función calcularLongitud
 		longitud = calcularLongitud();	
 	}
 	//------------------------
@@ -118,11 +116,17 @@ public class Lado {
 	}
 	
 	//Calcula la longitud euclideana del lado.
+	
+	/**
+	 * Calcula la longitud de un lado de acuerdo a la distancia euclidiana
+	 * @return longitud del lado
+	 */
+
+	//      Para calcular potencias, use la función Math.pow y para raíces use
+	//      Math.sqrt. Con el fin de hacerlo lo más general posible, trate de usar
+	//      un ciclo for sobre la dimension del espacio.
+
 	private double calcularLongitud( ) {
-		//TODO: Calcular la longitud del lado de acuerdo a la distancia euclideana.
-		//      Para calcular potencias, use la función Math.pow y para raíces use
-		//      Math.sqrt. Con el fin de hacerlo lo más general posible, trate de usar
-		//      un ciclo for sobre la dimension del espacio.
 		double longitud = 0.0;
 		double y = 0.0;
 		for(int j = 0; j < puntos[0].getDimensionEspacio() ; j++) {
